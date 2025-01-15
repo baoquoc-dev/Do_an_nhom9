4b3049907469ea963f7681df29055b1067dd38c9
Bảo Quốc <baoquocdevelop@gmail.com>
2025-01-15 14:39:26 +0700

    Upload

---
+++ data/products.js
@@ -0,0 +1,394 @@
+var list_products = [
+    {
+      "name": "iPhone 14 Pro Max",
+      "company": "iPhone",
+      "img": "img/products/ip14prm.jpg",
+      "price": "31.990.000",
+      "star": 5,
+      "rateCount": 154,
+      "promo": {
+        "name": "iPhone",
+        "value": "10"
+      },
+      "detail": {
+        "screen": "Super Retina XDR, 6.7', OLED",
+        "os": "iOS 16",
+        "camara": "48 MP",
+        "camaraFront": "12 MP",
+        "cpu": "Apple A16 Bionic",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "microUSB": "Không hỗ trợ",
+        "battery": "4323 mAh"
+      },
+      "masp": "App0"
+    },
+    {
+      "name": "iPhone 13",
+      "company": "iPhone",
+      "img": "img/products/ip13.jpg",
+      "price": "22.990.000",
+      "star": 4,
+      "rateCount": 320,
+      "promo": {
+        "name": "iPhone",
+        "value": "0"
+      },
+      "detail": {
+        "screen": "Super Retina XDR, 6.1', OLED",
+        "os": "iOS 15",
+        "camara": "12 MP",
+        "camaraFront": "12 MP",
+        "cpu": "Apple A15 Bionic",
+        "ram": "4 GB",
+        "rom": "128 GB",
+        "microUSB": "Không hỗ trợ",
+        "battery": "3240 mAh"
+      },
+      "masp": "App1"
+    },
+    {
+      "name": "iPhone 12",
+      "company": "iPhone",
+      "img": "img/products/ip12.jpg",
+      "price": "18.490.000",
+      "star": 4,
+      "rateCount": 450,
+      "promo": {
+        "name": "iPhone",
+        "value": "0"
+      },
+      "detail": {
+        "screen": "Super Retina XDR, 6.1', OLED",
+        "os": "iOS 14",
+        "camara": "12 MP",
+        "camaraFront": "12 MP",
+        "cpu": "Apple A14 Bionic",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "Không hỗ trợ",
+        "battery": "2815 mAh"
+      },
+      "masp": "App2"
+    },
+    {
+      "name": "iPhone SE (2020)",
+      "company": "iPhone",
+      "img": "img/products/ipse2020.jpg",
+      "price": "10.990.000",
+      "star": 4,
+      "rateCount": 170,
+      "promo": {
+        "name": "iPhone",
+        "value": "0"
+      },
+      "detail": {
+        "screen": "Retina HD, 4.7', IPS LCD",
+        "os": "iOS 13",
+        "camara": "12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A13 Bionic",
+        "ram": "3 GB",
+        "rom": "64 GB",
+        "microUSB": "Không hỗ trợ",
+        "battery": "1821 mAh"
+      },
+      "masp": "App3"
+    },
+    {
+      "name": "iPhone 11 Pro",
+      "company": "iPhone",
+      "img": "img/products/ip11pro.jpg",
+      "price": "26.990.000",
+      "star": 5,
+      "rateCount": 210,
+      "promo": {
+        "name": "iPhone",
+        "value": "5"
+      },
+      "detail": {
+        "screen": "Super Retina XDR, 5.8', OLED",
+        "os": "iOS 13",
+        "camara": "12 MP",
+        "camaraFront": "12 MP",
+        "cpu": "Apple A13 Bionic",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "Không hỗ trợ",
+        "battery": "3046 mAh"
+      },
+      "masp": "App4"
+    },
+    {
+      "name": "iPhone X",
+      "company": "iPhone",
+      "img": "img/products/ipx.jpg",
+      "price": "20.990.000",
+      "star": 4,
+      "rateCount": 380,
+      "promo": {
+        "name": "iPhone",
+        "value": "0"
+      },
+      "detail": {
+        "screen": "Super Retina, 5.8', OLED",
+        "os": "iOS 11",
+        "camara": "12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A11 Bionic",
+        "ram": "3 GB",
+        "rom": "64 GB",
+        "microUSB": "Không hỗ trợ",
+        "battery": "2716 mAh"
+      },
+      "masp": "App5"
+    },
+    {
+      "name": "iPhone 8 Plus",
+      "company": "iPhone",
+      "img": "img/products/ip8plus.jpg",
+      "price": "14.490.000",
+      "star": 4,
+      "rateCount": 290,
+      "promo": {
+        "name": "iPhone",
+        "value": "0"
+      },
+      "detail": {
+        "screen": "Retina HD, 5.5', IPS LCD",
+        "os": "iOS 11",
+        "camara": "12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A11 Bionic",
+        "ram": "3 GB",
+        "rom": "64 GB",
+        "microUSB": "Không hỗ trợ",
+        "battery": "2675 mAh"
+      },
+      "masp": "App6"
+    },
+    {
+      "name": "iPhone 7",
+      "company": "iPhone",
+      "img": "img/products/ip7.jpg",
+      "price": "9.490.000",
+      "star": 4,
+      "rateCount": 200,
+      "promo": {
+        "name": "iPhone",
+        "value": "0"
+      },
+      "detail": {
+        "screen": "Retina HD, 4.7', IPS LCD",
+        "os": "iOS 10",
+        "camara": "12 MP",
+        "camaraFront": "7 MP",
+        "cpu": "Apple A10 Fusion",
+        "ram": "2 GB",
+        "rom": "32 GB",
+        "microUSB": "Không hỗ trợ",
+        "battery": "1960 mAh"
+      },
+      "masp": "App7"
+    },
+    {
+      "name": "iPhone 6s Plus",
+      "company": "iPhone",
+      "img": "img/products/ip6splus.jpg",
+      "price": "7.990.000",
+      "star": 4,
+      "rateCount": 180,
+      "promo": {
+        "name": "iPhone",
+        "value": "0"
+      },
+      "detail": {
+        "screen": "Retina HD, 5.5', IPS LCD",
+        "os": "iOS 9",
+        "camara": "12 MP",
+        "camaraFront": "5 MP",
+        "cpu": "Apple A9",
+        "ram": "2 GB",
+        "rom": "16 GB",
+        "microUSB": "Không hỗ trợ",
+        "battery": "2750 mAh"
+      },
+      "masp": "App8"
+    },
+    {
+        "name": "MacBook Pro 16-inch M1 Pro",
+        "company": "MacBook",
+        "img": "img/products/macpro16.jpg",
+        "price": "56.990.000",
+        "star": 5,
+        "rateCount": 120,
+        "promo": {
+        "name": "Macbook",
+        "value": "5"
+        },
+        "detail": {
+        "screen": "16.2', Liquid Retina XDR",
+        "os": "macOS Monterey",
+        "cpu": "Apple M1 Pro chip",
+        "ram": "16 GB",
+        "rom": "512 GB SSD",
+        "microUSB": "Thunderbolt 4",
+        "battery": "100 WHr"
+        },
+        "masp": "Sam0"
+    },
+    {
+        "name": "MacBook Air M2",
+        "company": "MacBook",
+        "img": "img/products/macairm2.jpg",
+        "price": "29.990.000",
+        "star": 4,
+        "rateCount": 80,
+        "promo": {
+        "name": "Macbook",
+        "value": "0"
+        },
+        "detail": {
+        "screen": "13.6', Retina Display",
+        "os": "macOS Ventura",
+        "cpu": "Apple M2 chip",
+        "ram": "8 GB",
+        "rom": "256 GB SSD",
+        "microUSB": "Thunderbolt 3",
+        "battery": "52 WHr"
+        },
+        "masp": "Sam1"
+    },
+    {
+        "name": "MacBook Pro 14-inch M1 Pro",
+        "company": "MacBook",
+        "img": "img/products/macpro14.jpg",
+        "price": "42.990.000",
+        "star": 5,
+        "rateCount": 150,
+        "promo": {
+        "name": "Macbook",
+        "value": "10"
+        },
+        "detail": {
+        "screen": "14.2', Liquid Retina XDR",
+        "os": "macOS Monterey",
+        "cpu": "Apple M1 Pro chip",
+        "ram": "16 GB",
+        "rom": "512 GB SSD",
+        "microUSB": "Thunderbolt 4",
+        "battery": "70 WHr"
+        },
+        "masp": "Sam2"
+    },
+    {
+        "name": "MacBook Air M1",
+        "company": "MacBook",
+        "img": "img/products/macairm1.jpg",
+        "price": "24.990.000",
+        "star": 4,
+        "rateCount": 200,
+        "promo": {
+        "name": "Macbook",
+        "value": "0"
+        },
+        "detail": {
+        "screen": "13.3', Retina Display",
+        "os": "macOS Big Sur",
+        "cpu": "Apple M1 chip",
+        "ram": "8 GB",
+        "rom": "256 GB SSD",
+        "microUSB": "Thunderbolt 3",
+        "battery": "49.9 WHr"
+        },
+        "masp": "Sam3"
+    },
+    {
+        "name": "MacBook Pro 13-inch M2",
+        "company": "MacBook",
+        "img": "img/products/macpro13.jpg",
+        "price": "31.990.000",
+        "star": 4,
+        "rateCount": 120,
+        "promo": {
+        "name": "Macbook",
+        "value": "0"
+        },
+        "detail": {
+        "screen": "13.3', Retina Display",
+        "os": "macOS Monterey",
+        "cpu": "Apple M2 chip",
+        "ram": "8 GB",
+        "rom": "256 GB SSD",
+        "microUSB": "Thunderbolt 3",
+        "battery": "58 WHr"
+        },
+        "masp": "Sam4"
+    },
+    {
+        "name": "MacBook Air 13-inch M1",
+        "company": "MacBook",
+        "img": "img/products/macair13.jpg",
+        "price": "22.990.000",
+        "star": 4,
+        "rateCount": 180,
+        "promo": {
+        "name": "Macbook",
+        "value": "0"
+        },
+        "detail": {
+        "screen": "13.3', Retina Display",
+        "os": "macOS Big Sur",
+        "cpu": "Apple M1 chip",
+        "ram": "8 GB",
+        "rom": "256 GB SSD",
+        "microUSB": "Thunderbolt 3",
+        "battery": "49.9 WHr"
+        },
+        "masp": "Sam5"
+    },
+    {
+        "name": "MacBook Pro 13-inch M1",
+        "company": "MacBook",
+        "img": "img/products/macpro13m1.jpg",
+        "price": "28.990.000",
+        "star": 4,
+        "rateCount": 160,
+        "promo": {
+        "name": "Macbook",
+        "value": "0"
+        },
+        "detail": {
+        "screen": "13.3', Retina Display",
+        "os": "macOS Big Sur",
+        "cpu": "Apple M1 chip",
+        "ram": "8 GB",
+        "rom": "256 GB SSD",
+        "microUSB": "Thunderbolt 3",
+        "battery": "58 WHr"
+        },
+        "masp": "Sam6"
+    },
+    {
+        "name": "MacBook Pro 15-inch Retina",
+        "company": "MacBook",
+        "img": "img/products/macpro15.jpg",
+        "price": "45.990.000",
+        "star": 5,
+        "rateCount": 75,
+        "promo": {
+        "name": "Macbook",
+        "value": "5"
+        },
+        "detail": {
+        "screen": "15.4', Retina Display",
+        "os": "macOS Catalina",
+        "cpu": "Intel Core i7",
+        "ram": "16 GB",
+        "rom": "512 GB SSD",
+        "microUSB": "Thunderbolt 3",
+        "battery": "76 WHr"
+        },
+        "masp": "Sam7"
+    }
+  ]
